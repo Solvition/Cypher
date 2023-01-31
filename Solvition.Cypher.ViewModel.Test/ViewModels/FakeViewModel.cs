cad88bb84ab310da62357650a01d938e6c0a5390
Tim Hulshof <tim_hulshof@hotmail.com>
2023-01-31 16:51:19 -0700

    Create base class for view models

---
+++ Solvition.Cypher.ViewModel.Test/ViewModels/FakeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Solvition.Cypher.ViewModel.ViewModels;
+
+namespace Solvition.Cypher.ViewModel.Test.ViewModels
+{
+    internal class FakeViewModel : ViewModelBase
+    {
+        private int _intProperty = 0;
+
+        public int IntProperty
+        {
+            get { return _intProperty; }
+            set { _intProperty = value; RaisePropertyChanged(); }
+        }
+
+        public void RaisePropertyByName(string propertyName)
+        {
+            RaisePropertyChanged(propertyName);
+        }
+    }
+}
---
+++ Solvition.Cypher.ViewModel.Test/ViewModels/ViewModelBaseTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel;
+
+namespace Solvition.Cypher.ViewModel.Test.ViewModels
+{
+    [TestClass]
+    public class ViewModelBaseTests
+    {
+        [TestMethod]
+        public void PropertyChanged_WhenPropertyChanged_IsFiredOnlyOnce()
+        {
+            var propertyToChange = nameof(FakeViewModel.IntProperty);
+            var changeCount = 0;
+
+            var countHandler = new PropertyChangedEventHandler((s, e) =>
+            {
+                if (e.PropertyName == propertyToChange)
+                {
+                    changeCount++;
+                }
+            });
+
+            var viewModel = new FakeViewModel();
+            viewModel.PropertyChanged += countHandler;
+            viewModel.IntProperty++;
+
+            var expectedCount = 1;
+
+            Assert.AreEqual(expectedCount, changeCount);
+        }
+
+        [TestMethod]
+        public void PropertyChanged_WhenRaisedByName_IsFiredOnlyOnce()
+        {
+            var propertyToChange = nameof(FakeViewModel.IntProperty);
+            var changeCount = 0;
+
+            var countHandler = new PropertyChangedEventHandler((s, e) =>
+            {
+                if (e.PropertyName == propertyToChange)
+                {
+                    changeCount++;
+                }
+            });
+
+            var viewModel = new FakeViewModel();
+            viewModel.PropertyChanged += countHandler;
+            viewModel.RaisePropertyByName("IntProperty");
+
+            var expectedCount = 1;
+
+            Assert.AreEqual(expectedCount, changeCount);
+        }
+    }
+}

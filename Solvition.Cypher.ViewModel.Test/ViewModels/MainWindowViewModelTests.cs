a1ae41ef7a51a607b8c4f276ac7dabbd33a00a80
Tim Hulshof <tim_hulshof@hotmail.com>
2023-02-02 15:11:06 -0700

    Create new library from main window

---
+++ Solvition.Cypher.ViewModel.Test/ViewModels/MainWindowViewModelTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solvition.Cypher.ViewModel.ViewModels;
+
+namespace Solvition.Cypher.ViewModel.Test.ViewModels
+{
+    [TestClass]
+    public class MainWindowViewModelTests
+    {
+        [TestMethod]
+        public void NewFileCommand_CreatesNewLibraryViewModel()
+        {
+            var mainWindowViewModel = new MainWindowViewModel();
+            var initialLibraryViewModel = mainWindowViewModel.LibraryViewModel;
+
+            mainWindowViewModel.NewFileCommand.Execute(null);
+
+            Assert.AreNotEqual(initialLibraryViewModel, mainWindowViewModel.LibraryViewModel);
+        }
+    }
+}
--- Solvition.Cypher.ViewModel/ViewModels/MainWindowViewModel.cs
+++ Solvition.Cypher.ViewModel/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,18 @@
-﻿namespace Solvition.Cypher.ViewModel.ViewModels
+﻿using Solvition.Cypher.ViewModel.Commands;
+using System.Windows.Input;
+
+namespace Solvition.Cypher.ViewModel.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         private LibraryViewModel _libraryViewModel = new LibraryViewModel();
+
         public LibraryViewModel LibraryViewModel
         {
             get => _libraryViewModel;
             set { _libraryViewModel = value; RaisePropertyChanged(); }
         }
+
+        public ICommand NewFileCommand => new ActionCommand(() => LibraryViewModel = new LibraryViewModel());
     }
 }

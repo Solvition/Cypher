2ef7f61ff8d75ecb49df784e625732bed6386ede
Tim Hulshof <tim_hulshof@hotmail.com>
2023-02-02 21:26:11 -0700

    Create custom expander and utilize library

---
+++ Solvition.Cypher.ViewWpf/Controls/LibraryExpander.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Solvition.Cypher.ViewWpf.Controls
+{
+    /// <summary>
+    /// Interaction logic for LibraryExpander.xaml
+    /// </summary>
+    public partial class LibraryExpander : Expander
+    {
+        public LibraryExpander()
+        {
+            InitializeComponent();
+        }
+    }
+}

b3eda1f0d9fd738ca14cc58c61f076ea08fd88e2
Tim Hulshof <tim_hulshof@hotmail.com>
2023-02-04 20:52:21 -0700

    Modify expander behavior
    Expanders can only have one expanded at a time.
    Expanders fill the parent when expanded.

--- Solvition.Cypher.ViewWpf/Views/LibraryView.xaml.cs
+++ Solvition.Cypher.ViewWpf/Views/LibraryView.xaml.cs
@@ -11,5 +11,22 @@ namespace Solvition.Cypher.ViewWpf.Views
         {
             InitializeComponent();
         }
+
+        private void _expander_Expanded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var expander = sender as Expander;
+            if (expander is null)
+            {
+                return;
+            }
+
+            foreach (var child in _grid.Children)
+            {
+                if (child is Expander && child != sender)
+                {
+                    ((Expander)child).IsExpanded = false;
+                }
+            }
+        }
     }
 }
